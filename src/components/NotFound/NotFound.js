64dfeba248bd7ad083b81d6fb961cbfaa2e146d0
ZaimaSartajTaheri <67533852+ZaimaSartajTaheri@users.noreply.github.com>
2020-09-16 00:43:07 +0600

    first commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const NotFound = () => {
+    return (
+        <div class="text-center">
+           <h1>Page not found...404 Error!!!</h1> 
+        </div>
+    );
+};
+
+export default NotFound;
